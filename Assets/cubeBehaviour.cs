e14170604f2f22773c842be051aecdd5ba5179a2
Anthony Moles <acmoles@gmail.com>
2016-07-18 22:07:50 +0100

    Added animation tester cubeBehaviour

---
+++ Assets/cubeBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeBehaviour : MonoBehaviour {
+
+    public int speed = 1;
+    Animator CubeAnim;
+
+	// Use this for initialization
+	void Awake () {
+        CubeAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnimStarter ();
+	}
+
+    void AnimStarter ()
+    {
+        bool CheckKey = Input.GetKeyDown("space");
+        CubeAnim.SetBool("DoAction", CheckKey);
+        CubeAnim.speed = speed;
+    }
+}
